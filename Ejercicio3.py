fad637994352b84c57f469dcf91d0420bfc39024
chavezdiego <diego.chavez.barja@gmail.com>
2017-09-12 22:00:37 -0300

    Ejercicios de Repaso

---
+++ Ejercicio3.py
@@ -0,0 +1,30 @@
+"""
+3- Dado un struct de alumno que contiene un nombre de 30 caracteres, una edad
+que es un entero, un curso que es un caracter y
+otra estructura adentro que respresenta sus notas, la cual es un vector de
+3 notas (3 posiciones). Struct nota va a tener un entero valor.
+a) Declare un vector con 50 alumnos.
+b) Haga una función que dado un alumno, retorne el promedio de sus notas.
+c) Dados dos alumnos informar si son del mismo curso.
+d) Dado un alumno y un curso, haga una función que cambie de curso a ese alumno.
+e) Haga una función que dado un vector de alumnos nos liste solo aquellos
+alumnos cuyo promedio de notas es mayor o igual a 9.
+f) Dado un vector de alumnos, ordenarlo por edad ascendente.
+g) Dado un alumno y un vector de 3 notas, asignarles esas notas a ese alumno.
+(es una función).
+h) Dado un vector de notas, ordenar el vector por el promedio de notas ascendente.
+i) Dado dos vectores de alumnos ordenados por nombre ascendente, crear un tercer
+vector con los datos de los dos primeros también ordenados por alumno
+(apareo de vectores).
+j) Dado un vector de alumnos ordenarlo por código de curso e imprimir por
+pantalla el promedio total del promedio por curso. (corte de control).
+"""
+
+a = 3
+b = 4
+
+print("""\
+Uso: algo [OPTIONS]
+-h
+-H nombrehost
+""")
