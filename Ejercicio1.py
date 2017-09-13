98ca0139b4ff6ae589827dcb86c709d3d7fe5747
chavezdiego <diego.chavez.barja@gmail.com>
2017-09-12 23:41:27 -0300

    Enunciados

---
+++ Ejercicio1.py
@@ -0,0 +1,2 @@
+# 1- Hacer una función que intercambie dos números enteros que recibe como 
+# parámetro tanto el prototipo como en el desarrollo.
---
+++ Ejercicio2.py
@@ -0,0 +1,2 @@
+# 2- Hacer una función que dados tres números enteros modifique el valor del
+# tercer parámetro (entero), con la suma de los dos primeros parámetros.
